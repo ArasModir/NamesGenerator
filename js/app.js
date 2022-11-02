09bc66f369d1f832c396c0ef141e0e8309796c7a
ArasModir <109582452+ArasModir@users.noreply.github.com>
2022-11-02 14:53:11 +0100

    Printing the Results

--- js/app.js
+++ js/app.js
@@ -8,27 +8,40 @@ function loadNames(e) {
     const genre = document.getElementById('genre').value;
     const amount = document.getElementById('quantity').value;
 
-    let url = 'https://uinames.com/api/?';
+    let url = 'https://jsonplaceholder.typicode.com/posts';
 
     if (origin !== '') {
-        url += `region=${origin}&`;
+        // url += `region=${origin}&`;
     }
 
     if (genre !== '') {
-        url += `gender=${genre}&`;
+        // url += `gender=${genre}&`;
     }
 
     if (amount !== '') {
-        url += `amount=${amount}&`;
+        // url += `amount=${amount}&`;
     }
+
+    console.log(url);
 
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', url, true);
     xhr.onload = function () {
-        if (this.status === 2000) {
+        if (this.status === 200) {
             const names = JSON.parse(this.responseText);
             console.log(names);
+
+            let html = '<h2>Generated Titles</h2>';
+            html += '<ul class="list">'
+            names.forEach(name => {
+                html += `
+                    <li>${name.title}</li>
+                `;
+            });
+
+            html += '</ul>'
+            document.querySelector('#result').innerHTML = html;
         }
     }
 
